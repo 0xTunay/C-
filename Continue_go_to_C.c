0aabbad83f374e237fa288fe81dfafc8e0c8e0b6
Ttunay <tigrus232323k@gmail.com>
2023-09-07 18:30:15 +0300

    go to C day 2.3

---
+++ Continue_go_to_C.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (void)
+{
+    char a = 'f';
+    int b = 1;
+    float c = 4.5f;
+    double d = 5.584;
+
+
+    printf("ggg, %d %d %f %f", a,b,c,d);
+    return 0;
+
+}
