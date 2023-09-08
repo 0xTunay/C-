09bad1208be317c0709127ef1dc28c628c2bd522
Ttunay <tigrus232323k@gmail.com>
2023-09-08 17:59:19 +0300

    go to C, day 3.1

--- Continue_go_to_C.c
+++ Continue_go_to_C.c
@@ -1,13 +1,36 @@
 #include <stdio.h>
 int main (void)
 {
-    char a = 'f';
-    int b = 1;
-    float c = 4.5f;
-    double d = 5.584;
+ /*
+    int a = 1;
+    int b = 2;
+    int c = a + b;
+    printf("c = %d\n",c);
+ */
 
-
-    printf("ggg, %d %d %f %f", a,b,c,d);
+/*
+    int a = 10;
+    int b = 5;
+    int c = a / b;
+    printf("c = %d\n",c ); 
     return 0;
+*/   
+/*
+    float a = 10;
+    float b = 4;
+    float c = a/b;
+    printf("c = %.2f\n",c);
+    return 0;
+*/   
+            // int a = 10;
+            // int b = 4;
+            // int c = 10%3;
+            // printf("c = %d\n", c);
+            // return 0;
 
+     float a = 10;
+     float b = 4;
+     float c = 10 / 4;
+     printf("c = %.3f\n",c);
+     return 0;       
 }
