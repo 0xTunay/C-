86461427991934f7066d04a04591a1afb55c5fb9
Ttunay <tigrus232323k@gmail.com>
2023-09-11 18:52:56 +0300

    %%

--- he.c
+++ he.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 int main(void)
 {
-    for (int i = 0; i < 9; i++){
-        printf("%d * %d = %d\n",i,i, i*i);
+//    for(int i = 0; i < 6; i++) {
+//        printf("hello work \n");
+//    }
+//
+//    int y = 6;
+//    while (y > 0){
+//        printf(" hello work \n");
+//        y--;
+//    }
+//    return 0;
+
+    for(int i = 11; i < 21; i+=2) {
+        printf("i = %d\n",i);
     }
     return 0;
 }
