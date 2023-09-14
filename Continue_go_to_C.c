5a5fa2c40df3418e7478dfb6866379bb411fafd9
Ttunay <tigrus232323k@gmail.com>
2023-09-14 18:53:25 +0300

    update brains

--- Continue_go_to_C.c
+++ Continue_go_to_C.c
@@ -1,37 +1,10 @@
 #include <stdio.h>
 int main (void)
 {
-    int n;
- /*
-    int a = 1;
-    int b = 2;
-    int c = a + b;
-    printf("c = %d\n",c,a,b);
- */
+    char name[10];
+    printf("name>");
+    scanf("%10s[^/n]",name);
 
-/*
-    int a = 10;
-    int b = 5;
-    int c = a / b;
-    printf("c = %d\n",c ); 
+    printf("name %s \n",name);
     return 0;
-*/   
-/*
-    float a = 10;
-    float b = 4;
-    float c = a/b;
-    printf("c = %.2f\n",c);
-    return 0;
-*/   
-            // int a = 10;
-            // int b = 4;
-            // int c = 10%3;
-            // printf("c = %d\n", c);
-            // return 0;
-
-     float a = 10;
-     float b = 4;
-     float c = 10 / 4;
-     printf("c = %.1f\n",c);
-     return 0;       
 }
