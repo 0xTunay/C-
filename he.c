38f2c93dfafb1b37d1c426b5ccfcb074bd12353e
Ttunay <tigrus232323k@gmail.com>
2023-09-18 19:00:19 +0300

    go to c, day 8

--- he.c
+++ he.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-#include <string.h>
 int main(void)
 {
-    int x;
-    double y;
-    scanf("%ld %d",&x, &y);
-    printf("x = %ld , y = %d\n",x,y);
+    int num = 116;
+    int *p_num = &num;
+    printf("%d\n",*p_num);
+
+    *p_num = 117;
+
+    printf("%d\n",*p_num);
     return 0;
 }
