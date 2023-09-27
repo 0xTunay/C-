3001212ab2536d00f0144f45b1df58c8f02ffc8d
Ttunay <tigrus232323k@gmail.com>
2023-09-27 18:48:09 +0300

    day nnn

--- he.c
+++ he.c
@@ -1,17 +1,5 @@
 #include <stdio.h>
 int main(void)
 {
-    int x = 212;
-    char y = 'Hello wrold';
-    float z = 3.14;
 
-    void *point;
-
-    point = &x;
-    printf("%d\n", *(int *)point);
-    point = &y;
-    printf("%c\n", *(char *)point);
-    point = &z;
-    printf("%.2f\n",*(float*)point);
-    return 0;
 }
