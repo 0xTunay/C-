b3edb12a851e2c91e083d32701c1c9ccc0bb2d19
Ttunay <tigrus232323k@gmail.com>
2023-10-05 17:52:23 +0300

    result function

--- function.c
+++ function.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 
-int number(const int x)
+int sum( int x,int y, char prada)
 {
-    printf("%d",x*2);
+    if (prada =='+'){
+        return x+y;
+    }
+    else if(prada == '-'){
+        return x-y;
+    }
+    else{
+        return 0;
+    }
 }
 int main (void)
 {
-    number(10);
+    int x = 30;
+    int y = 10;
+    char prada = 'g';
+    int relust = sum(x,y,'+');
+    int relust1 = sum(x,y,'-');
+    int relust2 = sum(x,y,'g');
+
+    printf("%d + %d = %d\n",x,y,relust);
+    printf("%d - %d = %d\n",x,y,relust1);
+    printf("%d\n",relust2);
+
+
+
+
 }
