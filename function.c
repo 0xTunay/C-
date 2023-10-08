4f12ea8e4c451bf956b81c1568290436da01bffa
Ttunay <tigrus232323k@gmail.com>
2023-10-08 17:08:09 +0300

    vim??

--- function.c
+++ function.c
@@ -1,15 +1,15 @@
+#include <signal.h>
 #include <stdio.h>
 
-int fibonachy(int n)
-{
-    if(n == 0 || n == 1) return n;
+int fibonachy(int n) {
+  if (n == 0 || n == 1)
+    return n;
 
-       return fibonachy(n-1)+ fibonachy(n-2);
-
+  return fibonachy(n - 1) + fibonachy(n - 2);
 }
-int main (void)
-{
-    int fib4 = fibonachy(4);
-
-    printf("%d",fib4);
+int main(void) {
+  int fib4 = fibonachy(4);
+  int fib3 = fibonachy(3);
+  printf("%d\n", fib4);
+  printf("%d", fib3);
 }
