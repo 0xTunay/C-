3d1ab10a0bea47e933126f23815a06ad83e15e71
Ttunay <tigrus232323k@gmail.com>
2023-11-19 15:25:35 +0300

    PC

---
+++ cp.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define US 0
+#define ENGLAND 1
+#define FRANCE 2
+
+#define ACTIVITY_COUNTRY ENGLAND
+
+#if ACTIVITY_COUNTRY == US
+    char curr[] = "dollar";
+#elif ACTIVITY_COUNTRY == ENGLAND
+    char curr[] = "pound";
+#elif ACTIVITY_COUNTRY == FRANCE
+    char curr[] = "franc";
+#endif    
+int main(void)
+{
+    printf("curr = %s\n", curr);
+    return 0;
+}
