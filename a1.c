485fed36b30bc91b47d72980c50abcc4824bc995
Ttunay <tigrus232323k@gmail.com>
2024-02-12 17:59:31 +0300

    add

---
+++ a1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct tag_vector {
+    double x;
+    double y;
+};
+int main()
+{
+    struct tag_vector* prt_v = malloc(sizeof(struct tag_vector ));
+    prt_v->x = 2.3;
+    prt_v->y = 2.3;
+
+    printf("x = %2.f, y = %2.f", prt_v->x,prt_v->y);
+
+    free(prt_v);
+}
---
+++ a2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define PRT_INT int*
+
+typedef int* PTF;
+
+int main()
+{
+    PRT_INT a,b; // int a* int b
+    PTF ptr_a,ptr_b; // int ptr_a* , int ptr_b*
+}
