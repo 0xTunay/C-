5dc7fee9c05fdf33bf274b64cde32e0158ce3c46
Ttunay <tigrus232323k@gmail.com>
2024-08-15 15:47:30 +0300

    USA

---
+++ struct.c
@@ -0,0 +1,16 @@
+#include <sys/socket.h>
+#include <stdio.h>
+
+struct {
+    char *fuck;
+} typedef nigger;
+
+
+
+int main (void)
+{
+    nigger *Obama;
+    Obama->fuck = "Hello America";
+
+    printf("%s\n",Obama->fuck);
+}
